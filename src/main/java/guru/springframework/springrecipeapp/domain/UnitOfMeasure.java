2892f6b8d70463aaef00daff13e080b873b858c1
henriquewallace <henrique.wallace@hotmail.com>
2023-08-10 22:19:23 -0300

    create UnitOfMeasure entity and create an one to one relationship with ingredient

---
+++ src/main/java/guru/springframework/springrecipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,39 @@
+package guru.springframework.springrecipeapp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @OneToOne
+    private Ingredient ingredient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String uom) {
+        this.description = uom;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+}
