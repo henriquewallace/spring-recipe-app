66033076a6f549db77c08145381fea64fe321e09
henriquewallace <henrique.wallace@hotmail.com>
2023-09-12 21:54:22 -0300

    display a recipe by id

---
+++ src/main/java/guru/springframework/springrecipeapp/controllers/RecipeController.java
@@ -0,0 +1,26 @@
+package guru.springframework.springrecipeapp.controllers;
+
+import guru.springframework.springrecipeapp.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static java.lang.Long.valueOf;
+
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findById(valueOf(id)));
+
+        return "recipe/show";
+    }
+}
